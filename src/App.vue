5764e0f110ee6bcdc0afcb8578cbef9ff2d1aedb
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2022-02-13 20:30:28 +0700

    Restructure project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
-<script setup></script>
-
 <template>
-  <header></header>
-
-  <main></main>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
 <style>
-@import "./assets/base.css";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
