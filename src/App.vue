1e12888e0947382944f52a0cea0fe55b0b4075f9
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2022-02-20 22:55:10 +0700

    Add Router Register page,Home page,Profile page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/profile">Profile</router-link> |
+    <router-link to="/register">Register</router-link>
   </div>
   <router-view />
 </template>
---
+++ src/components/WorkshopCard.vue
@@ -0,0 +1,45 @@
+template
+<template>
+  <div>
+    <img id="poster" src="@/assets/Poster.png" alt="" />
+    <div class="card_container">
+      <div>
+        <div>{{ topic || "Kick-off Startup by SU Entrepreneur Club" }}</div>
+        <div>
+          {{
+            description ||
+            "แชร์ประสบการณ์จากรุ่นพี่สู่รุ่นน้อง โดยนักศึกษาผู้ประกอบการ"
+          }}
+        </div>
+        <div>{{ registerEnd || "เหลือเวลารับสมัครอีก 3 วัน" }}</div>
+      </div>
+      <div>{{ point || "300 pt" }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    topic: String,
+    description: String,
+    registerEnd: String,
+    point: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card_container {
+  display: flex;
+  text-align: left;
+  background: pink;
+  border-radius: 8px;
+  box-shadow: 2px;
+}
+#poster {
+  height: 50vh;
+}
+</style>
