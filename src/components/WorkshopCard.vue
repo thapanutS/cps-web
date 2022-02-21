5031bb384cf31bce3a5a0cdd14898cfea1a94bf1
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2022-02-21 19:55:38 +0700

    Modify WorkshopCard

--- src/components/WorkshopCard.vue
+++ src/components/WorkshopCard.vue
@@ -1,35 +1,25 @@
-template
 <template>
   <div>
     <img id="poster" src="@/assets/Poster.png" alt="" />
     <div class="card_container">
       <div>
-        <div>{{ topic || "Kick-off Startup by SU Entrepreneur Club" }}</div>
+        <div>{{ topic }}</div>
         <div>
-          {{
-            description ||
-            "แชร์ประสบการณ์จากรุ่นพี่สู่รุ่นน้อง โดยนักศึกษาผู้ประกอบการ"
-          }}
+          {{ description }}
         </div>
-        <div>{{ registerEnd || "เหลือเวลารับสมัครอีก 3 วัน" }}</div>
+        <div>{{ registerEnd }}</div>
       </div>
-      <div>{{ point || "300 pt" }}</div>
+      <div>{{ point }} pt</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    topic: String,
-    description: String,
-    registerEnd: String,
-    point: String,
-  },
+  name: "WorkshopCard",
+  props: ["topic", "description", "registerEnd", "point"],
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .card_container {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="home">
-    <WorkshopCard />
+    <WorkshopCard
+      :topic="topic"
+      :description="description"
+      :registerEnd="registerEnd"
+      :point="point"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import WorkshopCard from "@/components/WorkshopCard.vue";
-
+import { ref } from "vue";
 export default {
   name: "Home",
   components: {
     WorkshopCard,
+  },
+  setup() {
+    const topic = ref("Kick-off Startup by SU Entrepreneur Club");
+    const description = ref(
+      "แชร์ประสบการณ์จากรุ่นพี่สู่รุ่นน้อง โดยนักศึกษาผู้ประกอบการ"
+    );
+    const registerEnd = ref("เหลือเวลารับสมัครอีก 3 วัน");
+    const point = ref("300");
+
+    return {
+      topic,
+      description,
+      registerEnd,
+      point,
+    };
   },
 };
 </script>
