4bfc62bbfe48879d206ecfbf06a48dbc1f8e5153
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2022-03-01 18:07:59 +0700

    Add FilterChip component

---
+++ src/components/FilterChip.vue
@@ -0,0 +1,29 @@
+<template>
+  <button :class="[`filter`, `px-2`, isSelected ? ` selected` : '']">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "FilterChip",
+  props: ["text", "isSelected"],
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.filter {
+  width: max-content;
+  min-width: 60px;
+  height: 100%;
+  min-height: 26px;
+  border-radius: 14px 14px 14px 14px;
+  font-size: 12px;
+  background-color: #f1f1f1;
+  color: black;
+  &.selected {
+    background-color: #404041;
+    color: white;
+  }
+}
+</style>
