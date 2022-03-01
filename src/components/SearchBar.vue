8ac04fad6e7cd3ed524367c073c4cd25f1510125
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2022-03-01 18:08:22 +0700

    Add emit function

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,23 +1,30 @@
 <template>
   <div>
     <div class="search">
-      <input type="text" placeholder="ค้นหาข้อมูล" />
-      <div class="icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="-2 -5 24 28"
-          stroke="white"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-      </div>
+      <input
+        type="text"
+        placeholder="ค้นหาข้อมูล"
+        :value="searchValue"
+        @input="$emit('update:searchValue', $event.target.value)"
+      />
+      <button @click="searchFunction()">
+        <div class="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="-2 -5 24 28"
+            stroke="white"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
+      </button>
     </div>
   </div>
 </template>
@@ -25,7 +32,8 @@
 <script>
 export default {
   name: "SearchBar",
-  props: ["input"],
+  props: ["searchValue", "searchFunction"],
+  emits: ["update:searchValue"],
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
