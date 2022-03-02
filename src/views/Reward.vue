77e23dfb73c9d32d70c6f8c1e68c9ee21d22b7e8
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2022-03-02 18:42:30 +0700

    Add Reward Page

--- src/views/Reward.vue
+++ src/views/Reward.vue
@@ -1,12 +1,78 @@
 <template>
-  <div class="reward">
-    <h1>This is an Reward page</h1>
+  <div class="reward bg-quaternary">
+    <!-- POINT BAR -->
+    <section
+      class="flex items-center justify-between px-4 py-3 bg-white point-bar sticky top-0 z-1"
+    >
+      <p>{{ studentId }}</p>
+      <p>{{ name }}</p>
+      <div class="flex items-center">
+        <p class="mr-2">
+          <b>100</b>
+        </p>
+        <img src="@/assets/reward/token.png" alt="" />
+      </div>
+    </section>
+
+    <!-- ITEM LIST  -->
+    <section class="mt-2 pt-2 bg-white h-screen">
+      <div class="mt-2" v-for="item in itemList" :key="item.id">
+        <ItemCard :name="item.name" :count="item.count" :point="item.point" />
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import ItemCard from "@/components/ItemCard.vue";
+export default {
+  data() {
+    return {
+      studentId: "07610451",
+      name: "Piyabute Chairiboon",
+      itemList: [
+        {
+          id: 1,
+          name: "Apple watch",
+          count: 6,
+          point: 15000,
+        },
+        {
+          id: 2,
+          name: "Apple watch",
+          count: 6,
+          point: 15000,
+        },
+        {
+          id: 3,
+          name: "Apple watch",
+          count: 6,
+          point: 15000,
+        },
+        {
+          id: 4,
+          name: "Apple watch",
+          count: 6,
+          point: 15000,
+        },
+      ],
+    };
+  },
+  components: {
+    ItemCard,
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .reward {
   height: 100%;
   min-height: 90vh;
+}
+.point-bar {
+  border-bottom: 1px solid #c7c7c7;
+  img {
+    width: 4vw;
+  }
 }
 </style>
