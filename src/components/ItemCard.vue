7674cb3117e848b0e1937966934340ba09d97e10
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2022-03-02 18:43:33 +0700

    Add component ItemCard

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="card-container bg-white flex items-center justify-between mx-2 rounded"
+  >
+    <div>
+      <img src="@/assets/reward/item.png" alt="" />
+    </div>
+    <div>
+      <div class="my-1">{{ name }}</div>
+      <div class="card-tube rounded-full px-3">เหลือ {{ count }} ชิ้น</div>
+    </div>
+    <div class="mr-2">
+      <div>{{ point }} Points</div>
+      <div>
+        <button class="bg-secondary text-white rounded px-2">Claim</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemCard",
+  props: ["img", "name", "count", "point"],
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss" scoped>
+.card-container {
+  border: 1px solid #c7c7c7;
+  img {
+    width: 75px;
+    height: 75px;
+    border-right: 1px solid #c7c7c7;
+  }
+}
+.card-tube {
+  background: #f8c749;
+}
+</style>
