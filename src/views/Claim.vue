935eb9b2c6873991deba40d5cb528feaa7101f0e
pybc <piyabute.c@gmail.com>
2022-03-05 21:09:31 +0700

    Add claim page
    create component claim card, create page claim, link page with navbar

--- src/views/Claim.vue
+++ src/views/Claim.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="claim">
-    <h1>This is an Claim page</h1>
+  <div class="reward bg-quaternary">
+    <!-- POINT BAR -->
+    <section
+      class="flex items-center justify-between px-4 py-3 bg-white point-bar sticky top-0 z-1"
+    >
+      <p>{{ studentId }}</p>
+      <p>{{ name }}</p>
+      <div class="flex items-center">
+        <p class="mr-2">
+          <b>100</b>
+        </p>
+        <img src="@/assets/reward/token.png" alt="" />
+      </div>
+    </section>
+
+    <!-- ITEM LIST  -->
+    <section class="mt-2 pt-2 bg-white h-screen">
+      <div class="mt-2" v-for="claim in claimList" :key="claim.id">
+        <ClaimCard
+          :name="claim.name"
+          :claimDate="claim.claimDate"
+          :status="claim.status"
+        />
+      </div>
+    </section>
   </div>
 </template>
 
+<script>
+import ClaimCard from "@/components/ClaimCard.vue";
+export default {
+  name: "Claim",
+  data() {
+    return {
+      studentId: "07610451",
+      name: "Piyabute Chairiboon",
+      claimList: [
+        {
+          id: 1,
+          name: "Apple watch",
+          claimDate: "12/02/22",
+          status: "WAITING",
+        },
+        {
+          id: 2,
+          name: "Apple watch",
+          claimDate: "12/02/22",
+          status: "CONFIRMED",
+        },
+        {
+          id: 3,
+          name: "Apple watch",
+          claimDate: "12/02/22",
+          status: "REJECTED",
+        },
+        {
+          id: 4,
+          name: "Apple watch",
+          claimDate: "12/02/22",
+          status: "CONFIRMED",
+        },
+      ],
+    };
+  },
+  components: {
+    ClaimCard,
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-.claim {
+.reward {
   height: 100%;
   min-height: 90vh;
 }
+.point-bar {
+  border-bottom: 1px solid #c7c7c7;
+  img {
+    width: 4vw;
+  }
+}
 </style>
-
-<script>
-export default {
-  name: "Claim",
-};
-</script>
