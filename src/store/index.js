62f80ccb0460d50c96f17395079bd0632cfed12d
pybc <piyabute.c@gmail.com>
2022-03-06 23:14:13 +0700

    Add mockup store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,43 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    eventSelected: {
+      id: "4",
+      name: "Kick-off Startup by SU Entrepreneur Club",
+      img: "imgsrc",
+      description:
+        "แชร์ประสบการณ์จากรุ่นพี่สู่รุ่นน้องแชร์ประสบการณ์จากรุ่นพี่สู่รุ่นน้องแชร์ประสบการณ์จากรุ่นพี่สู่รุ่นน้อง",
+      tags: ["Hackathon", "Workshop"],
+      location: "สถานที่อบรมคือ Zoom",
+      status: "OPEN",
+      point: 300,
+      registerEnd: "12/12/21",
+      eventStart: "06/03/22",
+      eventEnd: "07/03/22",
+    },
+    user: {
+      activeEvent: ["1", "2", "3"],
+      historyEvent: ["1", "2", "3"],
+    },
+  },
   mutations: {},
   actions: {},
+  getters: {
+    isRegister(state) {
+      let status = false;
+      state.user.activeEvent.forEach((element) => {
+        if (state.eventSelected.id === element) {
+          status = true;
+        }
+      });
+      state.user.historyEvent.forEach((element) => {
+        if (state.eventSelected.id === element) {
+          status = true;
+        }
+      });
+      return status;
+    },
+  },
   modules: {},
 });
