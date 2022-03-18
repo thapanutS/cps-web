ee56bd802502c7f8f790e9d3d9369e07df8312f5
Thapanut Saripat <thapanutsaripat@gmail.com>
2022-03-19 00:21:51 +0700

    Fix line utils

--- src/utils/line.js
+++ src/utils/line.js
@@ -1,106 +1,42 @@
-import { emptyPromise } from './common'
-import Config from '~/config'
-
-const { liff } = window
-
+// import Config from '~/config'
+import liff from "@line/liff";
 class LineUtil {
-  isInitiated = false
-  liffId = ''
-
-  /**
-   * @param {string} liffId - LIFF app ID
-   * @return {Promise}
-   */
-  init(liffId) {
-    if (Config.devTest) return
-
-    if (this.isInitiated) return emptyPromise()
-
-    return liff
-      .init({ liffId })
-      .then(() => {
-        this.isInitiated = true
-        this.liffId = liffId
-      })
-      .catch((err) => {
-        // eslint-disable-next-line no-console
-        console.error(err.code, err.message)
-      })
+  init() {
+    return liff.init(`${process.env.VUE_APP_LINE_LIFF_ID}`).catch((err) => {
+      console.error(err.code, err.message);
+    });
   }
 
   login(redirectUri) {
-    if (!liff.isLoggedIn()) {
-      liff.login(redirectUri ? { redirectUri } : null)
-    }
+    liff.ready
+      .then(() => {
+        if (liff.isLoggedIn()) {
+          this.getProfile();
+        } else {
+          liff.login(
+            redirectUri
+              ? {
+                  redirectUri,
+                }
+              : null
+          );
+        }
+      })
+      .catch((err) => {
+        console.log("Login error", err);
+      });
   }
-
-  logout() {
-    if (liff.isLoggedIn()) {
-      liff.logout()
-    }
-  }
-
-  async checkInitial() {
-    if (!this.isInitiated) {
-      await this.init(this.liff)
-    }
-  }
-
-  checkLogin() {
-    if (!liff.isInClient()) {
-      // (web login) in the LINE's in-app browser or external browser
-      this.login(window.location.href)
-    }
-  }
-
-  getOS(){
-    return liff.getOS()
-  }
-
-  /**
-   * @return {Promise} is resolved, the object containing the user's profile information
-   */
-  async getProfile() {
-    await this.checkInitial()
-    this.checkLogin()
-    return liff.getProfile()
-  }
-
-  async getAccessToken() {
-    await this.checkInitial()
-    this.checkLogin()
-    return liff.getAccessToken()
-  }
-
-  async getIDToken() {
-    await this.checkInitial()
-    this.checkLogin()
-    return liff.getIDToken()
-  }
-
-  async getDecodedIDToken() {
-    await this.checkInitial()
-    this.checkLogin()
-    return liff.getDecodedIDToken()
-  }
-
-  async sendMessages(messages) {
-    const messagesToSend = Array.isArray(messages) ? messages : [messages]
-    await liff.sendMessages(messagesToSend)
-  }
-
-  openWindow(url, external) {
-    liff.openWindow({ url, external })
-  }
-
-  closeWindow() {
-    liff.closeWindow()
-  }
-
-  getVersion() {
-    return liff.getVersion()
+  getProfile() {
+    liff
+      .getProfile()
+      .then((profile) => {
+        this.$store.dispatch("user/setLineProfile", profile);
+      })
+      .catch((err) => {
+        console.log("getProfile error", err);
+      });
   }
 }
 
-const lineUtil = new LineUtil()
-export default lineUtil
+const lineUtil = new LineUtil();
+export default lineUtil;
