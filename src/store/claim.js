fbc36d744ad38b9f7019a61fbd407c580ea15826
pybc <piyabute.c@gmail.com>
2022-03-19 00:16:01 +0700

    integrate api in claim page

--- src/store/claim.js
+++ src/store/claim.js
@@ -2,11 +2,15 @@ import axios from "axios";
 
 const state = () => ({
   claimList: null,
+  claimCreated: null,
 });
 
 const getters = {
   getClaimList(state) {
     return state.claimList;
+  },
+  getClaimCreated(state) {
+    return state.claimCreated;
   },
 };
 
@@ -19,11 +23,28 @@ const actions = {
     commit("setClaimList", claimList.data);
     return claimList;
   },
+  async createClaimRequest({ commit }, payload) {
+    const claimCreated = await axios.post(
+      `${process.env.VUE_APP_API_URL}/claim/create`,
+      {
+        uid: payload.uid,
+        itemId: payload.itemId,
+        status: "WAITING",
+        claimDate: "22",
+      }
+    );
+    commit("setClaimCreated", claimCreated.data);
+    console.log("createClaimRequest res:", claimCreated.data);
+    return claimCreated.data;
+  },
 };
 
 const mutations = {
   setClaimList(state, claimList) {
     state.claimList = claimList;
+  },
+  setClaimCreated(state, claimCreated) {
+    state.claimCreated = claimCreated;
   },
 };
 
