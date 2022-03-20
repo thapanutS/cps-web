c66897451dc3009b19f548c41e19ca512629ae9d
Thapanut Saripat <thapanutsaripat@gmail.com>
2022-03-20 19:21:36 +0700

    modify store user

--- src/store/user.js
+++ src/store/user.js
@@ -4,8 +4,7 @@ import axios from "axios";
 const state = () => ({
   userProfile: {},
   eventList: {},
-  // lineProfile: {}, // get data from line
-  lineVertifyInfo:{}, // Verify ID token from line
+  lineProfile: {}, // get data from line
 });
 
 // getters
@@ -16,12 +15,9 @@ const getters = {
   getEventList() {
     return state.eventList;
   },
-  getLineVertifyInfo() {
-    return state.lineVertifyInfo;
+  getLineProfile(state) {
+    return state.lineProfile;
   },
-  // getLineProfile(state) {
-  //   return state.lineProfile;
-  // },
 };
 
 // actions
@@ -47,21 +43,13 @@ const actions = {
     );
     commit("setEventList", eventList.data);
   },
-  async verifyIDToken({ commit }, idToken) {
-    console.log('verifyIDToken user store');
-    console.log('ID Token : ',idToken);
-    const channelId = process.env.VUE_APP_LINE_LIFF_CHANNEL_ID;
-    const vertifyInfo = await axios.post(
-      `${process.env.VUE_APP_API_URL}/user/verify/idToken/`, /// รอ แบงค์ต่อ API
-      { idToken, channelId }
+  async checkRegister(uid) {
+    const registerStatus = await axios.get(
+      `${process.env.VUE_APP_API_URL}/event/list/${uid}`
     );
-    console.log('Vertify Info : ',vertifyInfo);
-    commit("setVertifyInfo", vertifyInfo.data);
+    return registerStatus.status === "200" ? true : false;
   },
-  // async setLineProfile({ commit }, lineProfile) {
-  //   console.log("setLineProfile in Store");
-  //   commit("setLineProfile", lineProfile);
-  // },
+
 };
 
 // mutations
@@ -70,19 +58,11 @@ const mutations = {
     state.userProfile = userProfile; // for data from register
   },
   setLineProfile(state, lineProfile) {
-    state.userProfile = lineProfile; /// for data from line
+    state.lineProfile = lineProfile; /// for data from line
   },
   setEventList(state, eventList) {
     state.eventList = eventList;
   },
-  setVertifyInfo(state, vertifyInfo) {
-    console.log('setVertifyInfo : ',vertifyInfo);
-    state.lineVertifyInfo = vertifyInfo; /// for data from vertify ID token (Line)
-    console.log('State Line Vertify Info : ',state.lineVertifyInfo);
-  },
-  // setLineProfile(state, lineProfile) {
-  //   state.lineProfile = lineProfile;
-  // },
 };
 
 export default {
