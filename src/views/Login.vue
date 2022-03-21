5df9e0db42fe7414cca83d80d4df0f3a71595b01
Thapanut Saripat <thapanutsaripat@gmail.com>
2022-03-21 23:42:16 +0700

    Create Login.vue
    Add login page

---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+import lineUtils from "@/utils/line.js";
+export default {
+  setup() {
+    const router = useRouter();
+
+    await lineUtils.init();
+    await lineUtils.login();
+
+    const lineProfile = localStorage.getItem(
+      `LIFF_STORE:${process.env.VUE_APP_LINE_LIFF_ID}:decodedIDToken`
+    );
+
+    const registerStatus = await store.dispatch(
+      "user/checkRegister",
+      lineProfile.sub
+    );
+
+    if (registerStatus === false) {
+      router.push("/register");
+    } else {
+      router.push("/home");
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
