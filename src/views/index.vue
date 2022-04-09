3128402ebae48189cc37a8c9d51d2791a81d703a
Thapanut Saripat <thapanut@emetworks.com>
2022-04-10 00:27:19 +0700

    Modify flow work of index page for router to register page or home page

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,39 +1,84 @@
 <template>
-  <div><b>Index Page</b></div>
+  <div>
+    <loading
+      :active="isLoading"
+      :can-cancel="false"
+      :is-full-page="true"
+      color="#15C5B5"
+      loader="dots"
+      :height="60"
+      :width="70"
+      :opacity="1"
+      background-color
+      :lock-scroll="true"
+    ></loading>
+    <b>Index Page</b>
+  </div>
 </template>
 
 <script>
 import lineUtils from "@/utils/line.js";
+import config from "../../config";
+import { mapGetters } from "vuex";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 export default {
+  components: {
+    Loading,
+  },
+  data() {
+    return {
+      isLoading: false,
+      uid: "",
+      displayName: "",
+      pictureUrl: "",
+    };
+  },
   created() {
     console.log("Create starting");
     this.fetchData();
+  },
+  computed: {
+    ...mapGetters({
+      userProfile: "user/getUserProfile",
+    }),
   },
   methods: {
     async fetchData() {
       console.log("Line init");
       await lineUtils.init();
-      this.isRegistered();
+      if (!config.dev_status) {
+        console.log("!config.dev_status");
+        await lineUtils.login();
+        this.isLoading = true;
+        const lineProfile = JSON.parse(
+          localStorage.getItem(
+            `LIFF_STORE:${config.line.liff_id}:decodedIDToken`
+          )
+        );
+        (this.uid = lineProfile.sub),
+          (this.displayName = lineProfile.name),
+          (this.pictureUrl = lineProfile.picture);
+      } else {
+        console.log("config.dev_status");
+        (this.uid = "Ua28a9b8f51a7009c0361e8b9c3df674a"),
+          (this.displayName = "Book"),
+          (this.pictureUrl =
+            "https://www.img.in.th/images/33fdad6bd60ea49e0aea95f7eb751d32.png");
+      }
+      await this.isRegistered();
     },
     async isRegistered() {
-      await lineUtils.login();
-      const lineProfile = JSON.parse(localStorage.getItem(
-        `LIFF_STORE:${process.env.VUE_APP_LINE_LIFF_ID}:decodedIDToken`
-      ));
-      console.log("lineProfile : ", lineProfile);
-      // console.log("lineProfile : ", JSON.parse(lineProfile));
-      if (lineProfile) {
-      console.log("lineProfile.sub : ", lineProfile.sub);
-
-        const registerStatus = await this.$store.dispatch(
-          "user/checkRegister",
-          // "Ua28a9b8f51a7009c0361e8b9c3df674c"
-          lineProfile.sub
-        );
-        console.log(registerStatus);
-        registerStatus
-          ? this.$router.push("/homepage")
-          : this.$router.push("/register");
+      console.log("isRegistered");
+      await this.$store.dispatch("user/getUserProfile", this.uid);
+      if (this.userProfile !== null) {
+        console.log("user is registered");
+        this.isLoading = false;
+        this.$router.push("/");
+      } else {
+        console.log("user is not registered");
+        this.isLoading = false;
+        this.$router.push("/register");
       }
     },
   },
