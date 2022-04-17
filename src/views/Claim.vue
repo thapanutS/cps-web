bf0d736a345ed6960ab3b085dccd29b78f047383
Thapanut Saripat <thapanut@emetworks.com>
2022-04-17 21:20:41 +0700

    Change class name

--- src/views/Claim.vue
+++ src/views/Claim.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="reward bg-quaternary">
+  <div class="claim bg-quaternary">
     <Layout />
     <!-- POINT BAR -->
     <loading
@@ -93,7 +93,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.reward {
+.claim {
   height: 100%;
   min-height: 90vh;
 }
