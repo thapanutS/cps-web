98657e3c4219e56429ab24c4d54d9806c8a2263b
Thapanut Saripat <thapanut@emetworks.com>
2022-04-17 14:10:59 +0700

    Clean code

--- src/views/Claim.vue
+++ src/views/Claim.vue
@@ -74,13 +74,14 @@ export default {
 
     const fetchData = async (userProfile) => {
       isLoading.value = true;
-      let userID = null;
-      if (!config.dev_status) {
-        await lineUtils.initAndLogin();
-        userID = userProfile.value.uid;
-      } else {
-        userID = "Ua28a9b8f51a7009c0361e8b9c3df674a";
-      }
+      const userID = !config.dev_status ? userProfile.value.uid : "Ua28a9b8f51a7009c0361e8b9c3df674a";
+      !config.dev_status ? await lineUtils.initAndLogin() : '';
+      // if (!config.dev_status) {
+      //   await lineUtils.initAndLogin();
+      //   userID = userProfile.value.uid;
+      // } else {
+      //   userID = "Ua28a9b8f51a7009c0361e8b9c3df674a";
+      // }
       await store.dispatch(
         "claim/fetchClaimListByUid",
         userID
